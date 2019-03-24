9936f96ae5d256087d61f865b6ff716125999d8f
Roberto Rodriguez <roberto@rodriguezartavia.com>
2019-03-24 10:05:32 -0600

    fc

--- config.js
+++ config.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  url: "https://soyayan.com",
+  title: "Soy Ayan",
+  subtitle: "tetjfjfjfjfjfjffjfjfj"d,
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 1,
+  googleAnalyticsId: "UA-73379983-2",
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about"
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: "Contact me",
+      path: "/pages/contacts"
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "Roberto Rodriguez",
+    photo: "/photo.jpg",
+    bio: "sds dsd sdsddsddddffdffffffffffffffffff",
     contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
+      email: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "#",
+      vkontakte: "#"
     }
   }
 };
